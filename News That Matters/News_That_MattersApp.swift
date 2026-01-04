164b19d9bddf4c172a7bf53d78939d763892f760
ludik <bakhanetssimon@gmail.com>
2026-01-04 16:46:19 +0700

    Initial Commit

---
+++ News That Matters/News_That_MattersApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_That_MattersApp.swift
+//  News That Matters
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct News_That_MattersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ News That MattersTests/News_That_MattersTests.swift
@@ -0,0 +1,16 @@
+//
+//  News_That_MattersTests.swift
+//  News That MattersTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct News_That_MattersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
